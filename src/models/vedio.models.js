a62810f98c60d305cb7c74e1cfe82f05af41e718
madhuramkulshrestha123 <128170524+madhuramkulshrestha123@users.noreply.github.com>
2024-05-30 11:38:15 +0530

    Vedio and user jwt and bcrypt

---
+++ src/models/vedio.models.js
@@ -0,0 +1,41 @@
+import mongoose, { Aggregate } from "mongoose";
+import mongooseAggregatePaginate from "mongoose-aggregate-paginate-v2";
+const videoSchema = new Schema(
+{
+    videofile:{
+        type:String, //cloudinary
+        required: true
+    },
+    thumbnail: {
+        type:String, //cloudinary
+        required: true
+    },
+    title:{
+        type:String, 
+        required: true
+    },
+    description: {
+        type:String, 
+        required: true
+    },
+    duration: {
+        type:String, //cloudinary
+        required: true
+    },
+    view: {
+        type:Number,
+        default:0
+    },
+    isPublished:{
+        type:Boolean,
+        default:true
+    },
+    owner: {
+        type: Schema.Types.ObjectId,
+        ref:"User"
+    } 
+},
+{timestamps:true});
+
+videoSchema
+export const Video = mongoose.model("Video",videoSchema)
