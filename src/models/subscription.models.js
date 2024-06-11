8b92ce16a8ae530664e601b93a06af98f9e790f6
madhuramkulshrestha123 <128170524+madhuramkulshrestha123@users.noreply.github.com>
2024-06-11 18:55:41 +0530

    "Get user-profile" controller

--- src/models/subscription.models.js
+++ src/models/subscription.models.js
@@ -1,12 +1,12 @@
 import mongoose, { Schema, mongo } from "mongoose";
 
 const subscriptionSchema = new Schema({
-    subscriber:{
+    subscriber:{        //who is subscribing
         type:Schema.Types.ObjectId,
         ref:"User"
     },
     channel:{
-        type:Schema.Types.ObjectId,
+        type:Schema.Types.ObjectId, // whom subscriber is subscribing
         ref:"User"
     }
 })
