4ab9038b332c66a3fad5ad8ec8d9216c65ce4897
madhuramkulshrestha123 <128170524+madhuramkulshrestha123@users.noreply.github.com>
2024-06-23 10:25:55 +0530

    playlist Routes

--- src/routes/playlist.routes.js
+++ src/routes/playlist.routes.js
@@ -1,26 +1,16 @@
 import { Router } from 'express';
-import {
-    addVideoToPlaylist,
-    createPlaylist,
-    deletePlaylist,
-    getPlaylistById,
-    getUserPlaylists,
-    removeVideoFromPlaylist,
-    updatePlaylist,
-} from "../controllers/playlist.controller.js"
+import {addVideoToPlaylist,createPlaylist,deletePlaylist,getPlaylistById,getUserPlaylists,removeVideoFromPlaylist,updatePlaylist,} from "../controllers/playlist.controller.js"
 import {verifyJWT} from "../middlewares/auth.middleware.js"
 
 const router = Router();
 
 router.use(verifyJWT); 
 
-router.route("/").post(createPlaylist)
+router.route("/create_playlist").post(createPlaylist)
 
-router
-    .route("/:playlistId")
-    .get(getPlaylistById)
-    .patch(updatePlaylist)
-    .delete(deletePlaylist);
+router.route("/:playlistId").get(getPlaylistById)
+router.route("/:playlistId").patch(updatePlaylist)
+router.route("/:playlistId").delete(deletePlaylist)
 
 router.route("/add/:videoId/:playlistId").patch(addVideoToPlaylist); //add
 router.route("/remove/:videoId/:playlistId").patch(removeVideoFromPlaylist); //remove
